823e0cf477bac1b54fa71bcb9b85cf1588b797d0
jaytai2040 <jayyr2040@gmail.com>
2021-07-21 10:12:01 +0800

    set up holiday controller

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 3003;
 
+// Controllers/Routes
+const holidaysController = require("./controllers/holidays.js");
+app.use("/holidays", holidaysController);
+
+
 app.listen(PORT, () => {
   console.log("🎉🎊", "celebrations happening on port", PORT, "🎉🎊");
 });
